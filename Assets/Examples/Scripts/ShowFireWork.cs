b04d94a6a1fbce2355f4419993ee66af82287b2b
JasonStarkGit <stktr8@gmail.com>
2020-11-27 11:18:20 +1100

    Flag and Firework
    added in animated flags as well as a fire work effect which plays after golf ball hit in triggers also adjusted golf club distance to made it easier to reach the golf ball

---
+++ Assets/Examples/Scripts/ShowFireWork.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowFireWork : MonoBehaviour
+{
+    public GameObject firework;
+  
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Start Fire work now");
+        StartFireWork();
+    }
+
+    private void StartFireWork()
+    {
+        firework.SetActive(true);
+       // transform.GetChild(0).gameObject.SetActive(true);
+       // transform.GetChild(1).gameObject.SetActive(true);
+        
+    }
+}
